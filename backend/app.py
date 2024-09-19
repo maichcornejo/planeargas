7c2a4d92c5b1b04e9999c8e6c9205d608f709261
meli <melipacheco256@gmail.com>
2024-09-19 16:40:27 -0300

    actualizacion

--- backend/app.py
+++ backend/app.py
@@ -7,4 +7,8 @@ def home():
     return "¡Bienvenidos al backend de Planear Gas!"
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     app.run(host='0.0.0.0', port=8080)
+=======
+    app.run(host='0.0.0.0', port=8080)
+>>>>>>> 366618d8 (primer actualizacion melli)
---
+++ frontend/cli/nombre-del-componente/nombre-del-componente.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nombre-del-componente',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      nombre-del-componente works!
+    </p>
+  `,
+  styles: ``
+})
+export class NombreDelComponenteComponent {
+
+}
